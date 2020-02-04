c4d5177a3edb9cf98456e43d4a21d8e4718854a2
EC2 Default User <ec2-user@ip-172-31-82-78.ec2.internal>
2020-02-04 15:00:38 +0000

    even_word

--- even_word.py
+++ even_word.py
@@ -1,2 +1,18 @@
-def even_word():
-    pass
+def even_word(string):
+    chars = {}
+    makeEven = 0
+    for char in string:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+            
+    for char in chars:
+        if chars[char] % 2 != 0:
+            makeEven += chars[char] % 2 
+    return makeEven
+    
+print(even_word('potato'))
+print(even_word('aaabbbccc'))
+print(even_word('aaaa'))
+print(even_word('aabbbb'))
